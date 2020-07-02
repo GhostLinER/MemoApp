604d05064f2e0894edd48ca9fb8eea4a71e683d1
Jace Intanai (B60) <jace.int.work@gmail.com>
2020-07-02 14:18:33 +0700

    fix Camera fail_1

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,71 +1,74 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
 
-const Camera = ({ navigation }) => {
-    
-    takePicture = async function (camera) {
-        const options = { quality: 0.5, base64: true };
-        const data = await camera.takePictureAsync(options);
-        //  eslint-disable-next-line
-        console.log(data.uri);
-    };
+const Cameras = ({ navigation }) => {
 
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+  
+    useEffect(() => {
+        console.log("Here");
+        (async () => {
+            console.log("In");
+            // console.log(Permissions.askAsync(Permissions.CAMERA));
+            console.log("CHECK");
+            // const { status }  = await Camera.requestPermissionsAsync();
+            const permission_roll = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+            console.log(permission_roll.status);
+            const permission  = await Permissions.askAsync(Permissions.CAMERA);
+            console.log(permission.status);
+            console.log("SURE");
+            setHasPermission({ hasPermission: permission.status === 'granted'});
+            console.log(hasPermission);
+            console.log("END");
+        })();
+      }, []);
+
+    // useEffect
+    console.log(hasPermission)
+
+    if (hasPermission === null || hasPermission === undefined) {
+      return (
+            <View>
+                <Text style={{ fontSize: 86}}>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ERROR!!!!!!!!!!!!!!!!!!!!!!!!</Text>
+            </View>);
+    }
+    else if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
     return (
-        <View style={styles.container}>
-            <RNCamera
-                style={styles.preview}
-                type={RNCamera.Constants.Type.back}
-                flashMode={RNCamera.Constants.FlashMode.on}
-                androidCameraPermissionOptions={{
-                    title: 'Permission to use camera',
-                    message: 'We need your permission to use your camera',
-                    buttonPositive: 'Ok',
-                    buttonNegative: 'Cancel',
-                }}
-                androidRecordAudioPermissionOptions={{
-                    title: 'Permission to use audio recording',
-                    message: 'We need your permission to use your audio',
-                    buttonPositive: 'Ok',
-                    buttonNegative: 'Cancel',
-                }}
-            >
-                {/* {({ camera, status, recordAudioPermissionStatus }) => {
-                    if (status !== 'READY') return <PendingView />;
-                    return (
-                    <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                        <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
-                        <Text style={{ fontSize: 14 }}> SNAP </Text>
-                        </TouchableOpacity>
-                    </View>
-                    );
-                }} */}
-            </RNCamera>
-        </View>
-    );
+      <View style={{ flex: 1 }}>
+        <Camera style={{ flex: 1 }} type={type}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+              }}
+              onPress={() => {
+                setType(
+                    type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                );
+                useEffect
+              }}>
+              <Text style={{ fontSize: 168, marginBottom: 100, color: 'white' }}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+    )
 
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
-});
 
-export default Camera;
+export default Cameras;
